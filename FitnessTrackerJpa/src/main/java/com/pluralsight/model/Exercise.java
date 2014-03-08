47decc902faf12b5c33e07312ea45830cd635e83
iarielduarte <iariel.javauser@gmail.com>
2014-03-08 02:15:54 -0300

    update master

--- FitnessTrackerJpa/src/main/java/com/pluralsight/model/Exercise.java
+++ FitnessTrackerJpa/src/main/java/com/pluralsight/model/Exercise.java
@@ -1,28 +1,63 @@
 package com.pluralsight.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.annotations.ManyToAny;
 import org.hibernate.validator.constraints.Range;
 
-
+@Entity
+@Table(name="exercise")
 public class Exercise {
 	
+	
+	@Id
+	@GeneratedValue
+	@Column(name="exercise_id")
+	private Long id;
+	
 	@Range(min = 1, max = 120)
+	@Column(name="minutes")
 	private int minutes;
 	
 	@NotNull
+	@Column(name="activity")
 	private String activity;
+	
+	@ManyToOne
+	private Goal goal;
 
 	public String getActivity() {
 		return activity;
+	}
+
+	public Goal getGoal() {
+		return goal;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public int getMinutes() {
+		return minutes;
 	}
 
 	public void setActivity(String activity) {
 		this.activity = activity;
 	}
 
-	public int getMinutes() {
-		return minutes;
+	public void setGoal(Goal goal) {
+		this.goal = goal;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public void setMinutes(int minutes) {
--- FitnessTrackerJpa/src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
+++ FitnessTrackerJpa/src/main/java/com/pluralsight/repository/GoalRepositoryImpl.java
@@ -1,23 +1,50 @@
 package com.pluralsight.repository;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
+import javax.persistence.NamedQueries;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Goal;
+import com.pluralsight.model.GoalReport;
 
 @Repository("goalRepository")
 public class GoalRepositoryImpl implements GoalRepository {
 
 	@PersistenceContext
 	private EntityManager em;
-	
+
 	public Goal save(Goal goal) {
 		em.persist(goal);
 		em.flush();
 		return null;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Goal> loadAll() {
+		//-- Para usar con createQuery
+//		Query query = em.createQuery("Select g from Goal g");
+
+		//-- Para usar con @NamedQueries
+		TypedQuery<Goal> query = em.createNamedQuery(Goal.FIND_ALL_GOALS, Goal.class);
+		return query.getResultList();
+	}
+
+	public List<GoalReport> findAllGoalReports() {
+
+		//-- Para usar con createQuery
+//		Query query = em.createQuery("Select new com.pluralsight.model.GoalReport(g.minutes, e.minutes, e.activity) "
+//				+ "from Goal g, Exercise e where g.id = e.goal.id");
+
+		//-- Para usar con @NamedQueries
+		TypedQuery<GoalReport> query = em.createNamedQuery(Goal.FIND_GOAL_REPORTS, GoalReport.class);
+		return query.getResultList();
 	}
 
 }
