5c2ae916b3497b3c6844af0d1d41a0f4a9ab25c6
iarielduarte <iariel.javauser@gmail.com>
2014-12-02 00:56:58 -0300

    proyecto con ejemplo de inyeccion de dependencia por constructor

---
+++ gm/spring-concurso-talento-02/src/main/java/concursantes/EjecucionException.java
@@ -0,0 +1,15 @@
+package concursantes;
+
+/**
+ * Esta clase se utiliza para indicar cualquier exception
+ * @author Ariel Duarte
+ *
+ */
+
+public class EjecucionException extends RuntimeException{
+	
+	public EjecucionException(){}
+	
+	public EjecucionException(String msg){super(msg);}
+
+}
---
+++ gm/spring-concurso-talento-02/src/test/java/test/TestConcursoTalento.java
@@ -0,0 +1,45 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import concursantes.Concursante;
+import concursantes.Malabarista;
+
+public class TestConcursoTalento {
+
+	private static Log logger = LogFactory
+			.getLog("Running  TestConcursoTalento");
+	private Concursante malabarista1;
+	private Concursante malabarista2;
+
+	@Before
+	public void before() {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext(
+				"applicationContext.xml");
+
+		malabarista1 = (Concursante) ctx.getBean("solei");
+		malabarista2 = (Concursante) ctx.getBean("soleiRecitador");
+	}
+
+	@Test
+	public void testMalabarista() {
+		logger.info("Inicio de ejecutar Malabarista");
+		int noPelotas = 10;
+		malabarista1.ejecutar();
+		assertEquals(noPelotas, ((Malabarista) malabarista1).getPelotas());
+		logger.info("Fin de ejecutar Malabarista");
+		logger.info("Inicio de ejecutar Malabarista Recitador");
+		noPelotas = 15;
+		malabarista2.ejecutar();
+		assertEquals(noPelotas, ((Malabarista) malabarista2).getPelotas());
+		logger.info("Fin de ejecutar Malabarista Recitador");
+	}
+
+}
